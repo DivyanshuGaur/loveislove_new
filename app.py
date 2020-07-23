9fff2fe4c5398dd1f52332b593c63e49e4bc8b8a
Divy_ansh <48865853+DivyanshuGaur@users.noreply.github.com>
2020-07-23 22:18:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask,request,jsonify,render_template
 from easy_ocr import ocr_image
 
 from flask_cors import CORS
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer,PorterStemmer
+
 import os
 import joblib
 import nltk
@@ -30,7 +29,6 @@ lemmantizer=WordNetLemmatizer()
 ext_data=[]
 REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
 REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
-sw=stopwords.words('english')
 
 '''classifier=joblib.load('Models/LogReg.pickle')
 vectorizer=joblib.load('Models/Vectorizer.pickle')
@@ -53,43 +51,13 @@ def analayse():
         data='Hello world '
         '''for i in data_list:
             data+=i'''
-        sentiment=predict(data)
-        mp={'data':data,'sentiment':sentiment}
+        mp={'data':data,'sentiment':'positive'}
         return jsonify(mp)
 
 
 
 
 
-def getocr(fn):
-    ans = ocr_image(fn, service='youdao')
-    print(ans)
-    return ans
-
-
-
-def predict(data):
-
-    data=data.strip()
-    if(data=='' or data==' '):
-        return 'Random'
-
-    msg = re.sub('[^a-zA-Z]', ' ', data)
-    msg = msg.lower()
-    msg = msg.split()
-    msg = [lemmantizer.lemmatize(word) for word in msg if not word in sw]
-    msg = ' '.join(msg)
-    ext_data.append(msg)
-
-    vectors=vectorizer.transform(ext_data)
-    pred=classifier.predict(vectors)
-
-    print(pred[0])
-
-    if(pred[0]==0):
-        return 'Negative'
-    elif(pred[0]==1):
-        return 'Positive'
 
 
 
